c3b4e8b556c394007d7ed7ed3adef2248893ab2b
Ryan Faircloth <ryan@dss-i.com>
2023-07-25 07:45:35 -0400

    feat: initial

---
+++ python/box_enterprise_source/__init__.py
@@ -0,0 +1,120 @@
+import json
+import time
+import asyncio
+import orjson
+import os
+
+from boxsdk import Client, JWTAuth
+from boxsdk.exception import BoxException
+from boxsdk.object.events import EnterpriseEventsStreamType
+import requests
+import backoff
+
+from syslogng import LogSource
+from syslogng import LogMessage
+from syslogng import Logger
+from syslogng import Persist
+
+logger = Logger()
+
+config_path = os.environ.get("SEGWAY_CONFIG_PATH", "")
+    
+class EventStream(LogSource):
+    """Provides a syslog-ng async source for Microsoft Event hub"""
+
+    cancelled: bool = False
+    
+    
+    def init(self, options):
+        self._client = None
+        self.auth()
+        self.persist = Persist("EventStream", defaults={"stream_position": 0})
+        
+        return True
+    
+    def auth(self):
+            f = open('data.json')
+            self.auth_dict = json.load(f)
+            f.close()
+            try:
+                result=JWTAuth.from_settings_dictionary(self.auth_dict)
+            except (TypeError, ValueError, KeyError):
+                logger.error('Could not load JWT from settings dictionary')
+                return False
+            self._client = Client(result)
+
+    
+    def run(self):
+        """Simple Run method to create the loop"""
+        asyncio.run(self.receive_batch())
+
+    async def receive_batch(self):
+        params = {
+                    'limit': self._MAX_CHUNK_SIZE,
+                    'stream_type': EnterpriseEventsStreamType.ADMIN_LOGS,
+                    'stream_position': self.persist['stream_position']
+                }
+        timeout=5
+        while not self.cancelled:
+            # self.cancelled = True
+            # try:
+            box_response = self._get_events(params)
+            events = clean_event(box_response)
+            for event in box_response:
+                record_lmsg = LogMessage(event)
+                self.post_message(record_lmsg)
+
+            if box_response['next_stream_position'] and int(box_response['next_stream_position'])>0:
+                self.persist['stream_position'] = box_response['next_stream_position']
+                params['stream_position']=box_response['next_stream_position']
+
+                                        
+    @backoff.on_exception(backoff.expo,
+                    (requests.exceptions.Timeout,
+                    requests.exceptions.ConnectionError),max_time=300)
+    @backoff.on_predicate(backoff.expo, lambda x: x['entries'] == [],max_time=300)
+    def _get_events(self,params):
+        box_response = self._client.make_request(
+            'GET',
+            self._client.get_url('events'),
+            params=params,
+            timeout=30
+        )
+        result = box_response.json()
+        return result 
+        
+    
+
+    @staticmethod
+    def clean_event(source_dict: dict):
+        """
+        Delete keys with the value ``None``  or ```` (empty) string in a dictionary, recursively.
+        Remove empty list and dict objects
+
+        This alters the input so you may wish to ``copy`` the dict first.
+        """
+        # For Python 3, write `list(d.items())`; `d.items()` won’t work
+        # For Python 2, write `d.items()`; `d.iteritems()` won’t work
+        for key, value in list(source_dict.items()):
+            if value is None:
+                del source_dict[key]
+            elif isinstance(value, str) and value in ("", "None", "none"):
+                del source_dict[key]
+            elif isinstance(value, str):
+                if value.endswith("\n"):
+                    value = value.strip("\n")
+
+                if value.startswith('{"'):
+                    try:
+                        value = orjson.loads(value)
+                        EventStream.clean_event(value)
+                        source_dict[key] = value
+                    except orjson.JSONDecodeError:
+                        pass
+            elif isinstance(value, dict) and not value:
+                del source_dict[key]
+            elif isinstance(value, dict):
+                EventStream.clean_event(value)
+            elif isinstance(value, list) and not value:
+                del source_dict[key]
+        return source_dict  # For convenience
